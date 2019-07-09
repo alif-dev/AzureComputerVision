ac09d404aa3a692b1abeb5ffa1c122666a8c0c06
alif-dev <alifm.dev@gmail.com>
2019-07-09 23:02:52 +0700

    fixing a bug2

--- index.php
+++ index.php
@@ -39,9 +39,11 @@ if (isset($_POST['submit'])) {
     //echo "<br />";
 ?>
 
+<!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Azure Computer Vision</title>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
     <script type="text/javascript">
@@ -57,62 +59,6 @@ if (isset($_POST['submit'])) {
           reader.readAsDataURL(input.files[0]);
           document.getElementById("description").innerHTML="";
         };
-    </script>
-    <!-- Analyzing image file -->
-    <script type="text/javascript">
-        $(document).ready(function () {
-            // **********************************************
-            // *** Update or verify the following values. ***
-            // **********************************************
-            // Replace <Subscription Key> with your valid subscription key.
-            var subscriptionKey = "adcc7abd04c341189aa26b49ed5e7001";
-            // You must use the same Azure region in your REST API method as you used to
-            // get your subscription keys. For example, if you got your subscription keys
-            // from the West US region, replace "westcentralus" in the URL
-            // below with "westus".
-            //
-            // Free trial subscription keys are generated in the "westus" region.
-            // If you use a free trial subscription key, you shouldn't need to change
-            // this region.
-            var uriBase =
-            "https://kflowvision.cognitiveservices.azure.com/vision/v2.0/analyze";
-            // Request parameters.
-            var params = {
-                "visualFeatures": "Categories,Description,Color",
-                "details": "",
-                "language": "en",
-            };
-            // Display the image.
-            var sourceImageUrl = "<?php echo $blob->getUrl() ?>";
-            document.querySelector("#selectedImage").src = sourceImageUrl;
-            // Make the REST API call.
-            $.ajax({
-                url: uriBase + "?" + $.param(params),
-                // Request headers.
-                beforeSend: function(xhrObj){
-                    xhrObj.setRequestHeader("Content-Type","application/json");
-                    xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
-                },
-                type: "POST",
-                // Request body.
-                data: '{"url": ' + '"' + sourceImageUrl + '"}',
-            })
-            .done(function(data) {
-                // Show formatted JSON on webpage.
-                //$("#responseTextArea").val(JSON.stringify(data, null, 2));
-                // console.log(data);
-                // var json = $.parseJSON(data);
-                $("#description").text(data.description.captions[0].text);
-            })
-            .fail(function(jqXHR, textStatus, errorThrown) {
-                // Display error message.
-                var errorString = (errorThrown === "") ? "Error. " :
-                errorThrown + " (" + jqXHR.status + "): ";
-                errorString += (jqXHR.responseText === "") ? "" :
-                jQuery.parseJSON(jqXHR.responseText).message;
-                alert(errorString);
-            });
-        });
     </script>
 </head>
 <body>
